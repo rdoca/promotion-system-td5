5f022c2bbe7fc36f8852a9818ed985967a364c44
Henrique Morato <riquemorato@gmail.com>
2021-02-01 22:08:05 -0300

    gerar cupons para promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -12,15 +12,25 @@ class PromotionsController < ApplicationController
   end
 
   def create
-    promotion_params = params.require(:promotion).permit(:name, :description, 
+    promotion_params = params.require(:promotion).permit(:name, :description,
                                       :code, :discount_rate,
                                       :coupon_quantity, :expiration_date)
     @promotion = Promotion.new(promotion_params)
-    
+
     if @promotion.save
       redirect_to @promotion
     else
-      render 'new' 
+      render 'new'
     end
+  end
+
+  def generate_coupons
+    @promotion = Promotion.find(params[:id])
+    (1..@promotion.coupon_quantity).each do |number|
+      Coupon.create!(code: "#{@promotion.code}-#{'%04d' % number}",
+                     promotion: @promotion)
+    end
+    flash[:notice] = 'Cupons gerados com sucesso'
+    redirect_to @promotion
   end
 end
